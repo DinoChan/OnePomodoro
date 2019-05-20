0641fab28567a6f93eb8a5f3d195c219161eb4bb
Dino Chan <dino.chan@live.com>
2019-05-20 22:12:23 +0800

    更新一点UI

---
+++ OnePomodoro/OnePomodoro/Helpers/NegationBoolToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace OnePomodoro.Helpers
+{
+    public class NegationBoolToVisibilityConverter : BoolToObjectConverter
+    {
+        public NegationBoolToVisibilityConverter()
+        {
+            base.TrueValue = Visibility.Collapsed;
+            base.FalseValue = Visibility.Visible;
+        }
+    }
+}
