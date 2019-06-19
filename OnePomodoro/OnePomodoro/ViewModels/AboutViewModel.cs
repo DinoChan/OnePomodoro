1a9c705d1af9f60d1ed49b7f645a2909bddbe02e
DinoChan <dino.chan@live.com>
2019-06-19 10:56:18 +0800

    修改函数名

--- OnePomodoro/OnePomodoro/ViewModels/AboutViewModel.cs
+++ OnePomodoro/OnePomodoro/ViewModels/AboutViewModel.cs
@@ -20,16 +20,11 @@ namespace OnePomodoro.ViewModels
     // TODO WTS: Add other settings as necessary. For help see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/pages/settings.md
     public class AboutViewModel : ViewModelBase
     {
-        private ElementTheme _elementTheme = ThemeSelectorService.Theme;
-
-        public ElementTheme ElementTheme
-        {
-            get { return _elementTheme; }
-
-            set { SetProperty(ref _elementTheme, value); }
-        }
-
         private string _version;
+
+        public AboutViewModel()
+        {
+        }
 
         public string Version
         {
@@ -37,51 +32,6 @@ namespace OnePomodoro.ViewModels
 
             set { SetProperty(ref _version, value); }
         }
-
-        public string Publisher => Package.Current.PublisherDisplayName;
-
-        private ICommand _switchThemeCommand;
-
-        public ICommand SwitchThemeCommand
-        {
-            get
-            {
-                if (_switchThemeCommand == null)
-                {
-                    _switchThemeCommand = new DelegateCommand<object>(
-                        async (param) =>
-                        {
-                            ElementTheme = (ElementTheme)param;
-                            await ThemeSelectorService.SetThemeAsync((ElementTheme)param);
-                        });
-                }
-
-                return _switchThemeCommand;
-            }
-        }
-
-        //public Visibility FeedbackLinkVisibility => Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported() ? Visibility.Visible : Visibility.Collapsed;
-
-        //private ICommand _launchFeedbackHubCommand;
-
-        //public ICommand LaunchFeedbackHubCommand
-        //{
-        //    get
-        //    {
-        //        if (_launchFeedbackHubCommand == null)
-        //        {
-        //            _launchFeedbackHubCommand = new DelegateCommand(
-        //                async () =>
-        //                {
-        //                    // This launcher is part of the Store Services SDK https://docs.microsoft.com/en-us/windows/uwp/monetize/microsoft-store-services-sdk
-        //                    var launcher = Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.GetDefault();
-        //                    await launcher.LaunchAsync();
-        //                });
-        //        }
-
-        //        return _launchFeedbackHubCommand;
-        //    }
-        //}
 
         private ICommand _reviewCommand;
 
@@ -101,16 +51,17 @@ namespace OnePomodoro.ViewModels
                 return _reviewCommand;
             }
         }
-
-
-        public AboutViewModel()
-        {
-        }
-
+      
         public async Task InitializeAsync()
         {
             Version = GetVersion();
             await Task.CompletedTask;
+        }
+
+        public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            await InitializeAsync();
         }
 
         private string GetVersion()
@@ -120,12 +71,6 @@ namespace OnePomodoro.ViewModels
             var version = packageId.Version;
 
             return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
-        }
-
-        public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
-        {
-            base.OnNavigatedTo(e, viewModelState);
-            await InitializeAsync();
         }
     }
 }
