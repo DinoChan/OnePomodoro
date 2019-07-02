ebdce0673db9f5bc8306d44aa6e1aa59dd6c478b
DinoChan <dino.chan@live.com>
2019-07-02 16:51:05 +0800

    困了懒得写

---
+++ OnePomodoro/OnePomodoro/Helpers/TitleAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePomodoro.Helpers
+{
+    public class TitleAttribute : Attribute
+    {
+        public TitleAttribute(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; }
+    }
+}
