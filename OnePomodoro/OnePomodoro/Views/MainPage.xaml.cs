c706ba7dd7879b492ead19968eaeb37e203d8d0e
Dino Chan <dino.chan@live.com>
2019-08-01 22:02:54 +0800

    开始画中画模式

--- OnePomodoro/OnePomodoro/Views/MainPage.xaml.cs
+++ OnePomodoro/OnePomodoro/Views/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using OnePomodoro.PomodoroViews;
 using OnePomodoro.ViewModels;
 using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -24,7 +25,8 @@ namespace OnePomodoro.Views
 
         private void OnOptionsClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(OptionsPage));
+            SwitchCompactOverlayMode();
+           // Frame.Navigate(typeof(OptionsPage));
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -32,6 +34,7 @@ namespace OnePomodoro.Views
             base.OnNavigatedTo(e);
             //Window.Current.SetTitleBar(null);
             Window.Current.SetTitleBar(AppTitleBar);
+            
         }
 
 
@@ -51,6 +54,38 @@ namespace OnePomodoro.Views
             AppTitleBar.Height = coreTitleBar.Height;
             OptionsButton.Height= coreTitleBar.Height;
             //OptionsButton.Margin = new Thickness(0, 0, coreTitleBar.SystemOverlayRightInset, 0);
+        }
+
+        public static bool IsInCompactOverlayMode = false; //记录是否在画中画模式
+
+        public static bool IsCompactOverlayModeSupported  //确定当前设备是否支持制定的视图模式，这里就是画中画模式
+        {
+            get { return ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay); }
+        }
+
+        public static async void SwitchCompactOverlayMode()
+        {
+            bool _modeswitchstatus = false; // 切换模式成功指示器
+            if (IsInCompactOverlayMode)
+            {
+                _modeswitchstatus = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.Default);
+                //进入默认视图模式
+            }
+            else
+            {
+                if (IsCompactOverlayModeSupported)
+                {
+                    ViewModePreferences compactOptions = ViewModePreferences.CreateDefault(ApplicationViewMode.CompactOverlay);
+                    compactOptions.CustomSize = new Windows.Foundation.Size(350, 100); //调整画中画模式的窗口初始大小
+                    _modeswitchstatus = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay, compactOptions);
+                    //进入画中画模式
+                }
+                else
+                {
+                    //如果当前设备不支持画中画模式，则...（动作）
+                }
+            }
+            IsInCompactOverlayMode = _modeswitchstatus ? !IsInCompactOverlayMode : IsInCompactOverlayMode; //如果切换模式成功，则逆转这个值
         }
     }
 }
