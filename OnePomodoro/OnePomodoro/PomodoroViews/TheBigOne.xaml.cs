35a9ed6ea062634bd881f93daf36c632d23cd8e9
Dino Chan <dino.chan@live.com>
2019-08-02 22:24:44 +0800

    完成番茄钟

--- OnePomodoro/OnePomodoro/PomodoroViews/TheBigOne.xaml.cs
+++ OnePomodoro/OnePomodoro/PomodoroViews/TheBigOne.xaml.cs
@@ -14,6 +14,13 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Microsoft.Toolkit.Uwp.UI.Animations;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
+using Windows.UI;
+using System.Numerics;
+using Windows.UI.Composition;
+using System.Threading.Tasks;
+
 //https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
 
 namespace OnePomodoro.PomodoroViews
@@ -22,9 +29,74 @@ namespace OnePomodoro.PomodoroViews
     [Screenshot("")]
     public sealed partial class TheBigOne : PomodoroView
     {
+        private PointLight _inworkPointLight;
+        private PointLight _breakPointLight;
+
         public TheBigOne()
         {
             this.InitializeComponent();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await LayoutRoot.Fade(0.0f, duration: 0, delay: 0, easingMode: Windows.UI.Xaml.Media.Animation.EasingMode.EaseIn).StartAsync();
+            await LayoutRoot.Fade(1.0f, duration: 1500, delay: 0).StartAsync();
+            ShowTextShimming();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            StopTextShimming();
+        }
+
+
+
+        private void ShowTextShimming()
+        {
+            var compositor = Window.Current.Compositor;
+            if (_inworkPointLight == null)
+            {
+                var titleVisual = VisualExtensions.GetVisual(InworkShadow);
+                _inworkPointLight = compositor.CreatePointLight();
+
+                _inworkPointLight.Color = Color.FromArgb(255, 217, 17, 83);
+                _inworkPointLight.CoordinateSpace = titleVisual;
+                _inworkPointLight.Targets.Add(titleVisual);
+                _inworkPointLight.Offset = new Vector3(-(float)InworkShadow.ActualWidth * 4, (float)InworkShadow.ActualHeight / 2, 400.0f);
+            }
+            var offsetAnimation = compositor.CreateScalarKeyFrameAnimation();
+            offsetAnimation.InsertKeyFrame(1.0f, (float)InworkShadow.ActualWidth * 4, compositor.CreateLinearEasingFunction());
+            offsetAnimation.Duration = TimeSpan.FromMilliseconds(10000);
+            offsetAnimation.IterationBehavior = AnimationIterationBehavior.Forever;
+
+            _inworkPointLight.StartAnimation("Offset.X", offsetAnimation);
+
+
+
+            if (_breakPointLight == null)
+            {
+                var titleVisual = VisualExtensions.GetVisual(BreakShadow);
+                _breakPointLight = compositor.CreatePointLight();
+
+                _breakPointLight.Color = Color.FromArgb(255, 217, 17, 83);
+                _breakPointLight.CoordinateSpace = titleVisual;
+                _breakPointLight.Targets.Add(titleVisual);
+                _breakPointLight.Offset = new Vector3(-(float)InworkShadow.ActualWidth * 4, (float)InworkShadow.ActualHeight / 2, 400.0f);
+            }
+            offsetAnimation = compositor.CreateScalarKeyFrameAnimation();
+            offsetAnimation.InsertKeyFrame(1.0f, (float)InworkShadow.ActualWidth * 4, compositor.CreateLinearEasingFunction());
+            offsetAnimation.Duration = TimeSpan.FromMilliseconds(10000);
+            offsetAnimation.IterationBehavior = AnimationIterationBehavior.Forever;
+
+            _breakPointLight.StartAnimation("Offset.X", offsetAnimation);
+        }
+
+        private void StopTextShimming()
+        {
+            _inworkPointLight.StopAnimation("Offset.X");
+            _breakPointLight.StopAnimation("Offset.X");
         }
     }
 }
