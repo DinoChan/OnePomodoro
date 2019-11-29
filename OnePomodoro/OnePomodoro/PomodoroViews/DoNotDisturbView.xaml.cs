f565b11f3de05586d1c037a92ec51401098c9d68
DinoChan <dino.chan@live.com>
2019-11-29 16:47:40 +0800

    add do no disturb view

---
+++ OnePomodoro/OnePomodoro/PomodoroViews/DoNotDisturbView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using OnePomodoro.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace OnePomodoro.PomodoroViews
+{
+    [Title("Do Not Disturb")]
+    [Screenshot("/Assets/Screenshots/DoNotDisturb.png")]
+    public sealed partial class DoNotDisturbView 
+    {
+
+        public DoNotDisturbView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
