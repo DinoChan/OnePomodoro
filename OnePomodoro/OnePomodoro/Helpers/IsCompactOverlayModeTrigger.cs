9adc535875944ba006572a4ba5ac96fffa3d534d
DinoChan <dino.chan@live.com>
2019-12-12 16:32:02 +0800

    添加自定义的StateTrigger

---
+++ OnePomodoro/OnePomodoro/Helpers/IsCompactOverlayModeTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace OnePomodoro.Helpers
+{
+    public class IsCompactOverlayModeTrigger : StateTriggerBase
+    {
+        public IsCompactOverlayModeTrigger()
+        {
+            var view = ApplicationView.GetForCurrentView();
+            SetActive(view.IsFullScreenMode);
+            (Window.Current.Content as FrameworkElement).SizeChanged += OnSizeChanged;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var view = ApplicationView.GetForCurrentView();
+            SetActive(view.ViewMode == ApplicationViewMode.CompactOverlay);
+        }
+    }
+}
