0ec7c18aabf6ccf6771b787f144617c83fef5a9f
DinoChan <dino.chan@live.com>
2020-07-15 14:14:00 +0800

    添加Sqlite引用

---
+++ OnePomodoro/DataAccessLibrary/Period.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataAccessLibrary
+{
+    public class Period
+    {
+        public int Id { get; set; }
+
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+
+        public bool IsFocus { get; set; }
+
+        public bool HasFinished { get; set; }
+    }
+}
